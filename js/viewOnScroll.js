8808395226394e515971fd533f0654d33365ba7c
Richard Jang <jang.richard@gmail.com>
2024-03-10 01:16:52 -0600

    added js and styles and finished about

---
+++ js/viewOnScroll.js
@@ -0,0 +1,17 @@
+function viewOnScroll(renderIn, renderOut, identifier) {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.remove(renderOut);
+                entry.target.classList.add(renderIn);
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll(identifier);
+    hiddenElements.forEach((el) => observer.observe(el));
+}
+
+viewOnScroll('flyInRight', 'disappear', '.transformRight');
+viewOnScroll('flyInLeft', 'disappear', '.transformLeft');
+viewOnScroll('fadeIn', 'disappear', '.FadeIn');
