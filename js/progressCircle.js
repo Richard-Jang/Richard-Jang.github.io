7e17692d5fa6908b3ae73437bc4e99d5d180a366
Richard Jang <jang.richard7017@gmail.com>
2024-03-10 21:33:29 -0500

    replaced progress circle to progress bars

--- js/progressCircle.js
+++ js/progressCircle.js
@@ -1,25 +1,46 @@
-const rating = document.getElementsByClassName('ticks')[0];
-const tick = document.getElementsByClassName('tick');
 
-for (var i = 1; i < 100; i++) {
-    rating.innerHTML += "<div class='tick'></div>";
-    tick[i].style.transform = "rotate("+ 3.6 * i +"deg)";
-    tick[i].style.animationDelay = `${i/40}s`;
+
+rendered = false
+
+function viewOnScroll(renderIn, renderOut, identifier) {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting && !rendered) {
+                rendered = true
+                entry.target.classList.remove(renderOut);
+                entry.target.classList.add(renderIn);
+
+                const rating = document.getElementsByClassName('ticks')[0];
+                const tick = document.getElementsByClassName('tick');
+
+                for (var i = 1; i < 100; i++) {
+                    rating.innerHTML += "<div class='tick'></div>";
+                    tick[i].style.transform = "rotate("+ 3.6 * i +"deg)";
+                    tick[i].style.animationDelay = `${i/75}s`;
+                }
+
+                const counter = document.querySelector('.counter');
+                counter.innerText = 0;
+
+                const target = +counter.getAttribute('data-target');
+
+                const NumberCounter= () => {
+                    const value = +counter.innerText;
+                    if (value < target){
+                        counter.innerText = Math.ceil(value + 1);
+                        setTimeout(() => {
+                            NumberCounter()
+                        }, 20)
+                    }
+                }
+
+                NumberCounter()
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll(identifier);
+    hiddenElements.forEach((el) => observer.observe(el));
 }
 
-const counter = document.querySelector('.counter');
-counter.innerText = 0;
-
-const target = +counter.getAttribute('data-target');
-
-const NumberCounter= () => {
-    const value = +counter.innerText;
-    if (value < target){
-        counter.innerText = Math.ceil(value + 1);
-        setTimeout(() => {
-            NumberCounter()
-        }, 20)
-    }
-}
-
-NumberCounter()
+viewOnScroll('fadeIn', 'disappear', '.FadeInProgress');
