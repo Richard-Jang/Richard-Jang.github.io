e032a679df7930e8587de6e9da266d06800a2864
Richard Jang <jang.richard@gmail.com>
2024-03-12 23:13:31 -0500

    finished homepage

--- js/pausePlay.js
+++ js/pausePlay.js
@@ -1,24 +1,13 @@
-// Play <i class="bi bi-play-fill"></i> True when autoplay false
-// Pause <i class="bi bi-pause-fill"></i> True when autoplay true
-
-const pausePlay = (target, targetButton) => {
-
-    const targetElement = document.getElementById(target);
-    let targetAttribute = ""
-    targetElement.onload = () => {
-        targetAttribute = targetElement.getAttribute('data-bs-interval');
-    }
-    //let targetAttribute = targetElement.onload = () => {targetElement.getAttribute('data-bs-interval');}
-    let buttonElement = document.getElementById(targetButton);
-    console.log(targetAttribute);
-
-    if (targetAttribute == "3000") {
-        buttonElement.innerHTML = '<i class="bi bi-play-fill"></i>';
-        targetElement.setAttribute("data-bs-interval", "100");
+$("button").click(function() {
+    if ($(this).attr("id") === "pauseButton") {
+        $('#projectCarousel').carousel('pause');
+        $(this).attr("id", "playButton");
+        $('i', this).removeClass('bi-pause-fill')
+        $('i', this).addClass('bi-play-fill')
     } else {
-        buttonElement.innerHTML = '<i class="bi bi-pause-fill"></i>';
-        targetElement.setAttribute("data-bs-interval", "3000");
+        $('#projectCarousel').carousel('cycle');
+        $(this).attr("id", "pauseButton");
+        $('i', this).removeClass('bi-play-fill')
+        $('i', this).addClass('bi-pause-fill')
     }
-
-
-}
+});
--- js/progressChart.js
+++ js/progressChart.js
@@ -1,77 +1,50 @@
-/*
-import Chart from 'chart.js/auto';
-
-(async function() {
-    new Chart(document.getElementById('chart'), {
-        type: doughnut,
-        data: {
-            labels: ['Python', 'Swift', 'HTML/CSS/JavaScript', 'C#', 'Java', 'Miscellaneous'],
-            datasets: [{
-                label: 'Language Distribution',
-                data: [30, 15, 20, 5, 25, 5],
-                backgroundColor: ['#346e9e', '#fe502d', '#fed839', '#39008f', '#4f7896', '#6fbf73'],
-                hoverOffset: 4
-            }]
-        }
-    });
-})();
-*/
-
-/*
-import Chart from 'chart.js/auto'
-
-(async function() {
-    const data = [
-        { year: 2010, count: 10 },
-        { year: 2011, count: 20 },
-        { year: 2012, count: 15 },
-        { year: 2013, count: 25 },
-        { year: 2014, count: 22 },
-        { year: 2015, count: 30 },
-        { year: 2016, count: 28 },
-    ];
-
-    new Chart(
-        document.getElementById('acquisitions'),
-        {
-        type: 'bar',
-        data: {
-            labels: data.map(row => row.year),
-            datasets: [
+const pieChart = new Chart(document.getElementById('experiencePieChart'), {
+    type: 'doughnut',
+    data: {
+        labels: ['HTML/CSS/Javascript', 'Python', 'Java', 'C#', 'Swift'],
+        color: '#ffffff',
+        datasets: [
             {
-                label: 'Acquisitions by year',
-                data: data.map(row => row.count)
+                label: 'Percentage: ',
+                backgroundColor: ['#f9da53', "#3f76a5", '#f0931b', '#a073da', '#ff3828'],
+                data: [22, 34, 20, 9, 15]
             }
-            ]
-        }
-        }
-    );
-})();
-
-*/
-
-const rating = document.getElementsByClassName('rating')[0];
-const block = document.getElementsByClassName('block');
-
-for (var i = 1; i < 100; i++) {
-    rating.innerHTML += "<div class='block'></div>";
-    block[i].style.transform = "rotate("+ 3.6 * i +"deg)";
-    block[i].style.animationDelay = `${i/40}s`;
-}
-
-const counter = document.querySelector('.counter');
-counter.innerText = 0;
-
-const target = +counter.getAttribute('data-target');
-
-const NumberCounter= () => {
-    const value = +counter.innerText;
-    if (value < target){
-        counter.innerText = Math.ceil(value + 1);
-        setTimeout(() => {
-            NumberCounter()
-        }, 20)
+        ]
+    },
+    plugins: [ChartDataLabels],
+    options: {
+        plugins: {
+            legend: {
+                onClick: null,
+                display: true,
+                position: 'top',
+                align: 'center',
+                labels: {
+                    color: '#ffffff',
+                    font: {
+                        size: 14
+                    }
+                }
+            },
+            datalabels: {
+                formatter: function(value, context) {
+                    return value + "%";
+                },
+                color: '#ffffff',
+                font: {
+                    size: 20
+                }
+            }
+        },
+        layout: {
+            padding: 10
+        },
+        responsive: true,
+        maintainAspectRation: true,
+        aspectRatio: 1
     }
-}
+});
 
-NumberCounter()
+const updateChart = () => {
+    pieChart.update();
+}
